a9e83a0f2f4dd7b746bb755d469f9bdbf63b5f4f
Panescu Cosmin <1panescu.cosmin@gmail.com>
2022-11-10 18:12:29 +0200

    testing git contributions

---
+++ p14.c
@@ -0,0 +1,15 @@
+// Sa se scrie un program C in care se declara un vector cu maxim 20 de elemente de tip intreg. Numarul efectiv de elemente ale vectorul va fi preluat de la tastatura cu ajutorul unei functii care realizeaza si validarea acestuia: valorile admisibile sunt cuprinse in intervalul inchis [1, 20]. De asemenea se vor scrie functii pentru:
+// - citirea elementelor vectorului de numere întregi ;
+// - afisarea elementelor vectorului de numere întregi ;
+// - calculul sumei elementelor vectorului;
+// - calculul produsului elementelor vectorului;
+// - calculul mediei aritmetice a elementelor vectorului;
+// - calculul numarului de elementele negative din vector;
+// - calculul numarului de elementele pozitive din vector;
+// - calculul numarului de elementele egale cu zero din vector;
+// - calculul valorii minime dintre elementele vectorului;
+// - calculul valorii maxime dintre elementele vectorului;
+// - calculul sumei elementelor pozitive ale vectorului;
+// - calculul sumei elementelor negative ale vectorului;
+// - cautarea unei anumite valori în vector cu returnarea pozitiei pe care se gaseste; în cazul în care valoarea se afla de mai multe ori pe pozitii diferite, vom avea ca rezultat prima pozitie pe care acesta valoare a fost gasita ; daca valoarea cautata nu este gasita in vector functia va avea ca rezultat -1;
+
